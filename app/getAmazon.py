0dd538a87e70e06dc89c3aa4ea5488664686c088
v42me <i@v42.me>
2015-06-11 01:01:20 +0800

    get amazon 4 books everyday

--- app/getAmazon.py
+++ app/getAmazon.py
@@ -2,19 +2,40 @@ __author__ = 'oliverhuang'
 import requests
 from bs4 import BeautifulSoup
 
-website = "http://www.amazon.cn/gp/feature.html/ref=sa_menu_kindle_l3_f126758?ie=UTF8&docId=126758"
-# w1 = "http://www.google.com"
-res = requests.get(website)
-# print res.status_code
-soup = BeautifulSoup(res.content)
-# print res.content
-res = soup.find(class_="content")
-#print res
-for i in res.select("a")[-4:]:
-#get name
-	print i.text,i["href"]
-for j in res.find_all(class_="price"):
-#kindle price
-    print j.text
-for k in res.select("img"):
-    print k.src
+
+
+def getData():
+    items = list()
+    website = "http://www.amazon.cn/gp/feature.html/ref=sa_menu_kindle_l3_f126758?ie=UTF8&docId=126758"
+    # w1 = "http://www.google.com"
+    res = requests.get(website)
+    # print res.status_code
+    soup = BeautifulSoup(res.content)
+    # print res.content
+    res = soup.find(class_="content")
+    #print res
+    #for i in res.select("a")[-4:]:
+    #get name
+    #    print i.text
+    #    print i["href"]
+    #for j in res.find_all(class_="price"):
+    #kindle price
+    #    print j.text
+    #for k in res.select("img"):
+    #    print k.src
+    booknames = res.select("a")[-4:]
+    bookhrefs = res.select("a")[-4:]
+    bookprices = res.find_all(class_="price")
+    bookimages = res.select("img")
+    for num in range(4):
+        t = {
+                "bookname":booknames[num].text,
+                "bookhref":bookhrefs[num]["href"],
+                "bookprice":bookprices[num].text,
+                "bookimage":bookimages[num]["src"]
+                }
+        items.append(t)
+    return items
+
+if __name__ == "__main__":
+    getData()
--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 __author__ = 'oliverhuang'
 
 from app import app
+from getAmazon import getData
+from flask import render_template
+@app.route("/book")
+def book():
+    items = getData()
+    return render_template('index.html',items = items)
 
-
-app.route("/")
-
-
-def index():
+@app.route("/hello")
+def hello():
     return "hello"
